aaeabbef2edf7692a1a1f3a080a663e033b937e1
Oleksandr Slovak <slovak194@gmail.com>
2026-08-27 13:10:34 +0200

    migrate to nanobind, so jacobians work again

--- python/splinternano/dev_splinternano.py
+++ python/splinternano/dev_splinternano.py
@@ -1,25 +1,10 @@
 # %%
-import numpy as np
-from splinterpy import splinternano
-
-bspline = splinternano.BSpline("/home/slovak/splinter/python/splinternano/test.json")
-
-print(bspline)
-print(bspline.get_description())
-print(bspline.get_domain_upper_bound())
-print(bspline.get_domain_lower_bound())
-
-p = np.array((0.7, -0.1))
-print("\nbspline.eval:\n", bspline.eval(p))
-print("\bspline.eval_jacobian:\n", bspline.eval_jacobian(p))
-
-exit()
-
-# %%
 import os, sys
 
 import tempfile
 import splinterpy
+from splinterpy import splinternano
+
 import numpy as np
 from itertools import product
 np.set_printoptions(suppress=True)
@@ -46,34 +31,16 @@ bspline = splinterpy.bspline_interpolator(x, y, degree=3)
 p = np.array((0.7, -0.1))
 
 print("\nbspline.eval:\n", bspline.eval(p))
-print("\nbspline.eval_jacobian:\n", bspline.eval_jacobian(p))
+print("\nbspline.eval_jacobian (WRONG SHAPE):\n", bspline.eval_jacobian(p))
+
 
 with tempfile.NamedTemporaryFile() as temp:
     bspline.to_json(temp.name)
 
-    bspline_legacy = splinterpy.BSpline.from_json(temp.name)
-    print("\nbspline11.eval:\n", bspline_legacy.eval(p))
-    print("\nbspline11.eval_jacobian:\n", bspline_legacy.eval_jacobian(p))
+    bspline_nano = splinternano.BSpline(temp.name)
+
+    print("\nbspline11.eval:\n", bspline_nano.eval(p))
+    print("\nbspline11.eval_jacobian: (correct shape)\n", bspline_nano.eval_jacobian(p))
     # This evaluation of a jacobian is wrong. It gives (1, 2) shaped output, but it needs to be (3, 2) according the the shape of the input and output of the function.
     # That is the reason why I've added pybind11 version, as it suppose to output correctly shaped jacobian.
-
-# %%
-
-from splinterpy import splinterpy11
-
-temp = tempfile.NamedTemporaryFile()
-
-bspline.to_json(temp.name)
-
-bspline11 = splinterpy11.BSpline(temp.name)
-
-# %%
-
-bspline11.eval(p)
-
-print("\nbspline11.eval:\n", bspline11.eval(p))
-print("\nbspline11.eval_jacobian:\n", bspline11.eval_jacobian(p))
-
-
-# %%
 
